b39796fff8f0624036619ced72201c340ec8b776
leenath001 <leenathaniel001@gmail.com>
2025-01-19 19:49:15 -0500

    Update MCsimulator.py
    - Remove EV feature (in progress)
    - Set time to yearly to work with annualized drift & vol

--- MCsimulator.py
+++ MCsimulator.py
@@ -11,22 +11,18 @@ def format_number(num):
 def format_bank(num):
     return f"{num:,.2f}" 
 
-
 # 1 - PATH SIMULATION OF UNDERLYING (NORMAL SHOCKS, LOGN PRICES)
 
-## goals - use statistics to estimate mu, sigma based on time, dt (expected mu/sigma through each dt) 
-
-# data input (arr -> Motley Fool, vol -> Market Chameleon) IN PROGRESS
-arr = .1476
-vol = .134
-tradingdays = 21
-##
+# goals - use statistics to estimate mu, sigma (annualized)
+# IN PROGRESS
+# works better in years
 
 # estimation parameters
-iter = 5000
-time = 7
-S0 = 100
-paths = 50000
+iter = 1500
+days = 365
+time = days/365 
+S0 = 500
+paths = 30000
 mu = .05
 sig = .12
 dt = time/iter
@@ -38,13 +34,13 @@ array2 = numpy.ones((paths, iter))
 for rowind, row in enumerate(array):
     for colind, entry in enumerate(row[1:iter+1]):
         entry = 1 + (mu * dt + sig * m.sqrt(dt) * numpy.random.randn())
-        array[rowind,colind+1] = format_number(entry)
+        array[rowind,colind+1] = entry
         entry = array[rowind,colind+1]
         entry *= array2[rowind,colind]
         array2[rowind,colind+1] = entry
 
 # prices from last iteration of each path, along with random sampling of shocks
-prices = S0 * array2[:,iter-1] 
+prices = S0 * array2[:,iter-1]
 randint = random.randint(0,iter)
 shocks = array[:,randint]
 
@@ -63,9 +59,9 @@ print('Minimum of', f"{min:.2f}",', Maximum of', f"{max:.2f}")
 #   4 - P[strike >= underlying or underlying >= strike2]
 #   SET COUNT & SUM TO 0, strike2 > strike!!
 
-type = 4
-strike = 75
-strike2 = 150
+type = 3
+strike = 560
+strike2 = 600
 count = 0
 sum = 0
 
@@ -73,43 +69,46 @@ if type == 1:
     for p in prices:
         if p >= strike:
             count += 1
-            sum += p
+#            sum += (p - strike)
 elif type == 2:
     for p in prices:
         if p <= strike:
             count += 1
-            sum += p
+#            sum += (strike - p)
 elif type == 3:
     for p in prices:
         if p >= strike and p <= strike2:
             count += 1
-            sum += p
+#            sum += p
 elif type == 4:
     for p in prices:
         if p <= strike or p >= strike2:
             count += 1
-            sum += p
+#            sum += abs(p - strike)
 
 prob = count/paths
 ev = (1/paths) * sum
-print(f"{prob:.2%}", "trade is ITM. Expected value of", f"{ev:.2f}")
+print(f"{prob:.2%}", "trade is ITM.") 
+# "Expected value of", f"{ev:.2f}",'per contract.')
 
-'''
+
 # normality check
 plt.figure()
 #plt.hist(shocks, bins = 40, density = True)
-seaborn.distplot(shocks)
+plt.hist(shocks,bins = 60, density = True)
+seaborn.kdeplot(shocks)
 plt.xlabel('Shocks')
 plt.ylabel('Count')
 plt.title('Norm Dist of Shocks')
 plt.show()
-'''
+
 
 # log-normality check
 plt.figure()
 plt.hist(prices,bins = 60, density = True)
+seaborn.kdeplot(prices)
 #seaborn.distplot(prices)
-plt.xlim(0,max)
+plt.xlim(min,max)
 plt.xlabel('Price')
 plt.ylabel('Density')
 plt.title('Log-Norm Dist of Prices')
@@ -119,3 +118,6 @@ if type == 3 or type == 4:
 else:
     plt.axvline(x = strike, color = 'red')
 plt.show()
+
+
+
